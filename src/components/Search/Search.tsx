ad9cb530e5ce0e7dfb21d8f5b67796c4106367c0
Yaroslav-Borisov <yarpolar@gmail.com>
2023-12-22 12:48:46 +0300

    Добавит debounce для работы с поиском

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,20 +1,23 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { AppDispatch, RootState } from '../../store/store';
+import { useDispatch } from 'react-redux';
+import debounce from 'lodash.debounce';
+import { AppDispatch } from '../../store/store';
 import { cardsActions } from '../../store/cards.slice';
 import { useState } from 'react';
 
 export const Search = () => {
 	const dispatch = useDispatch<AppDispatch>();
-	const searchText = useSelector((state: RootState) => state.cards.search);
 	const [focus, setFocus] = useState(false);
 
-	const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		dispatch(cardsActions.changeSearchText(e.target.value.trim().toLowerCase()));
-	};
+	const onInputChange = debounce(
+		(e: React.ChangeEvent<HTMLInputElement>) => {
+			dispatch(cardsActions.changeSearchText(e.target.value.trim().toLowerCase()));
+		}
+		, 800);
+	
 
 	return (
 		<div className={focus ? 'search active' : 'search'}>
-			<input type="text" value={searchText} onChange={onInputChange} onFocus={() => setFocus(true)} onBlur={() => setFocus(false)} placeholder="Название блюда" />
+			<input type="text" onChange={onInputChange} onFocus={() => setFocus(true)} onBlur={() => setFocus(false)} placeholder="Название блюда" />
 			<img src="img/Search.svg" alt="Search" width="20" height="20" />
 		</div>
 	);
